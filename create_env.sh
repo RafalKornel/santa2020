6d0632c9d1ceb3dfce161dc3b6ced044482f586a
RafalKornel <rafal.kornel123@gmail.com>
2020-11-23 20:20:57 +0100

    Deployment commit!

--- create_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 -m venv app/venv
-source app/venv/bin/activate
-pip install -r requirements.txt
-deactivate
--- santa.py
+++ santa.py
@@ -1,10 +1,16 @@
 from app import create_app, db
 from app.models import User, Group
-from flask_migrate import Migrate
+import os
 
-app = create_app("development")
+app = create_app( os.environ.get("FLASK_CONFIG", "development"))
 
-migrate = Migrate(app, db)
+@app.cli.command("create_tables")
+def create_tables():
+    db.create_all()
+
+@app.cli.command("drop_tables")
+def drop_tables():
+    db.drop_all()
 
 @app.shell_context_processor
 def make_shell_context():
